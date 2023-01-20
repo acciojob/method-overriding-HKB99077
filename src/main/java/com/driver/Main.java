1104d2f761074a1cc59f0196c4b586ac329f5362
harsh <harshkumarbisen@gmail.com>
2023-01-20 19:10:13 +0530

    Solution3

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,29 @@
 package com.driver;
 
 public class Main {
-  
+    public static class A{
+        public String math(){
+            return "Invoking method from class A";
+        }
+
+
+    }
+    public static  class B extends A{
+@Override
+public String math(){
+    return "Method is overridden in Extendend class B";
+}
+    }
+
+
+    public static void main(String[] args){
+        B obj =new B();
+        obj.math();
+
+        B obj1 = new B();
+        obj1.math();
+
+
+    }
+
 }
